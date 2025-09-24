7188d23f6b6c06d4572a40cfc7ffd9d1486ee678
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-09-24 10:39:20 -0500

    refactor: Extract functions

--- command_map.go
+++ command_map.go
@@ -1,6 +1,47 @@
 package main
 
+import (
+	"bootdev-pokedex-go/internal/pokeapi"
+	"fmt"
+)
+
+func updateConfigURLs(config *Config, locationAreas pokeapi.BulkLocationArea) {
+	if locationAreas.Next != nil {
+		config.Next = locationAreas.Next
+	}
+	if locationAreas.Previous != nil {
+		config.Previous = locationAreas.Previous
+	}
+}
+
+func printLocationAreas(locationAreas pokeapi.BulkLocationArea) {
+	for _, area := range locationAreas.Results {
+		fmt.Println(area.Name)
+	}
+}
+
 func mapCallback(config *Config) error {
+	locationAreas, err := config.pokeapiClient.GetLocationAreas(config.Next)
+	if err != nil {
+		return err
+	}
+
+	updateConfigURLs(config, locationAreas)
+
+	printLocationAreas(locationAreas)
+
+	return nil
+}
+
+func mapbCallback(config *Config) error {
+	locationAreas, err := config.pokeapiClient.GetLocationAreas(config.Previous)
+	if err != nil {
+		return err
+	}
+
+	updateConfigURLs(config, locationAreas)
+
+	printLocationAreas(locationAreas)
 
 	return nil
 }
