0a447a9f018fa1173284cc5dd90ff09b482649a5
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-09-25 16:49:01 -0500

    feat: Add Reaping + concurrency safety

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -16,10 +16,12 @@ type cacheEntry struct {
 }
 
 func (c *Cache) Add(key string, val []byte) {
+	c.mutex.Lock()
 	c.entries[key] = cacheEntry{
 		createdAt: time.Now(),
 		val:       val,
 	}
+	c.mutex.Unlock()
 }
 
 func (c *Cache) Get(key string) ([]byte, bool) {
@@ -30,15 +32,28 @@ func (c *Cache) Get(key string) ([]byte, bool) {
 	return entry.val, ok
 }
 
-// func (c *Cache) reapLoop(interval time.Duration) {
-// 	ticker := time.NewTicker(interval)
-// }
+func (c *Cache) reap(ch <-chan time.Time, interval time.Duration) {
+	for timestamp := range ch {
+		for key, entry := range c.entries {
+			if timestamp.Sub(entry.createdAt).Milliseconds() > interval.Milliseconds() {
+				c.mutex.Lock()
+				delete(c.entries, key)
+				c.mutex.Unlock()
+			}
+		}
+	}
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	go c.reap(ticker.C, interval)
+}
 
 func NewCache(duration time.Duration) *Cache {
 	cache := Cache{
 		entries: map[string]cacheEntry{},
 		mutex:   sync.Mutex{},
 	}
-	// cache.reapLoop(5 * time.Second)
+	cache.reapLoop(5 * time.Second)
 	return &cache
 }
