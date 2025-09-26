8c7ecb45faefca8be32a329e62d3b30ca5b8bcb3
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-09-26 12:42:09 -0500

    fix: Add JSON parsing specs to struct types

--- internal/pokeapi/types_location_areas.go
+++ internal/pokeapi/types_location_areas.go
@@ -13,45 +13,45 @@ type NamedAPIResource struct {
 }
 
 type LocationArea struct {
-	Id                   int
-	Name                 string
-	GameIndex            int
-	EncounterMethodRates []EncounterMethodRate
-	Location             NamedAPIResource
-	Names                []Name
-	PokemonEncounters    []PokemonEncounter
+	Id                   int                   `json:"id"`
+	Name                 string                `json:"name"`
+	GameIndex            int                   `json:"game_index"`
+	EncounterMethodRates []EncounterMethodRate `json:"encounter_method_rates"`
+	Location             NamedAPIResource      `json:"location"`
+	Names                []Name                `json:"names"`
+	PokemonEncounters    []PokemonEncounter    `json:"pokemon_encounters"`
 }
 
 type EncounterMethodRate struct {
-	EncounterMethod NamedAPIResource
-	VersionDetails  []EncounterVersionDetails
+	EncounterMethod NamedAPIResource          `json:"encounter_method"`
+	VersionDetails  []EncounterVersionDetails `json:"version_details"`
 }
 
 type EncounterVersionDetails struct {
-	Rate    int
-	Version NamedAPIResource
+	Rate    int              `json:"rate"`
+	Version NamedAPIResource `json:"version"`
 }
 
 type Name struct {
-	Name     string
-	Language NamedAPIResource
+	Name     string           `json:"name"`
+	Language NamedAPIResource `json:"language"`
 }
 
 type PokemonEncounter struct {
-	Pokemon        NamedAPIResource
-	VersionDetails []VersionEncounterDetail
+	Pokemon        NamedAPIResource         `json:"pokemon"`
+	VersionDetails []VersionEncounterDetail `json:"version_details"`
 }
 
 type VersionEncounterDetail struct {
-	Version          NamedAPIResource
-	MaxChance        int
-	EncounterDetails []Encounter
+	Version          NamedAPIResource `json:"version"`
+	MaxChance        int              `json:"max_chance"`
+	EncounterDetails []Encounter      `json:"encounter_details"`
 }
 
 type Encounter struct {
-	MinLevel        int
-	MaxLevel        int
-	ConditionValues []NamedAPIResource
-	Chance          int
-	Method          NamedAPIResource
+	MinLevel        int                `json:"min_level"`
+	MaxLevel        int                `json:"max_level"`
+	ConditionValues []NamedAPIResource `json:"condition_values"`
+	Chance          int                `json:"chance"`
+	Method          NamedAPIResource   `json:"method"`
 }
